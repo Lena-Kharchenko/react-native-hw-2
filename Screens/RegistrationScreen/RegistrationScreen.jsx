6604d9070ff1789b124ff17370e20485984f3420
Lena Kharchenko <elenkakharchenko@gmail.com>
2023-06-27 18:51:41 +0300

    create RegistrationScreen

---
+++ Screens/RegistrationScreen/RegistrationScreen.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { View } from "react-native";
+
+const RegistrScreen = () => {
+  return (
+    <View>
+      <Image source={require("./images/PhotoBG.png")} />
+    </View>
+  );
+};
+
+export default RegistrScreen;
